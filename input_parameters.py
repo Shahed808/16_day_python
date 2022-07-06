67e0b652f7fb951137af27cf2db1e5dc64db0b5a
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-06 19:36:21 +0530

    Add files via upload

---
+++ input_parameters.py
@@ -0,0 +1,25 @@
+# t = input()
+# s = input()
+# print(t+s)                                                              # 12True , # 3.6True
+# print(t*s)
+
+
+
+# r = int (input('Any value'))
+# q =int (input('Any value'))
+# print(r+q)                                                              # 87
+
+# a = 5
+# b = 'Hello'
+# print(a*b)                                                              # HelloHelloHelloHelloHello
+
+
+# a = '3'
+# b = 'hel'
+# print(a+b)                                                                # 3hel
+
+
+
+w = float(input())
+v = float(input())
+print(w+v)                                                                      # 359.0
